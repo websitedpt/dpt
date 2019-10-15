1211bbf9e3349911fe518c07c8cdf3a378627925
theearthsmall <theearthsmall@gmail.com>
2019-10-15 21:43:51 +0700

    add

--- wp-content/themes/xediendpt/sidebar.php
+++ wp-content/themes/xediendpt/sidebar.php
@@ -15,7 +15,8 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 $taxonomy = 'danh-muc-san-pham';
 $terms = get_terms($taxonomy);
-
+$Catalog_all = get_category( get_query_var( 'cat' ) );
+$Catalog_slug = $Catalog_all->slug;
 ?>
 
 <div class="slider-bar">
@@ -41,36 +42,38 @@ $terms = get_terms($taxonomy);
         <?php } 
       } ?>
     <?php echo '</ul>'; endif; ?>
-  <div class="line-doted mb-2 mt-5"></div>
-  <h3 class="title-block text-capitalize mb-3">Tin tức</h3>
-  <?php
-    $args_news = array(
-    'category_name' => 'tin-tuc',
-    'post_type' => 'post',
-    'post_status' => 'publish',
-    'posts_per_page' => '7', 
-    );
-    $query_news = new WP_Query($args_news);
-    if ($query_news->have_posts() ) {
-      echo '<ul class="list-menu list-inline px-2 box-2">';  
-      while ($query_news->have_posts() ) {
-        $query_news->the_post();        
-        if ( has_post_thumbnail() ) { 
-          echo'<li><a href="' . get_the_permalink() .'"  title="' . get_the_title() .'">
-              <div class="media">
-                  ' .get_the_post_thumbnail( get_the_id(), 'full', array( 'class' =>'img-fluid') ).'                     
-                <div class="media-body pl-2">
-                  <h5 class="mt-0">' . get_the_title() .'</h5>
+  <?php if($Catalog_slug != 'tin-tuc') { ?>
+    <div class="line-doted mb-2 mt-5"></div>
+    <h3 class="title-block text-capitalize mb-3">Tin tức</h3>
+    <?php
+      $args_news = array(
+      'category_name' => 'tin-tuc',
+      'post_type' => 'post',
+      'post_status' => 'publish',
+      'posts_per_page' => '7', 
+      );
+      $query_news = new WP_Query($args_news);
+      if ($query_news->have_posts() ) {
+        echo '<ul class="list-menu list-inline px-2 box-2">';  
+        while ($query_news->have_posts() ) {
+          $query_news->the_post();        
+          if ( has_post_thumbnail() ) { 
+            echo'<li><a href="' . get_the_permalink() .'"  title="' . get_the_title() .'">
+                <div class="media">
+                    ' .get_the_post_thumbnail( get_the_id(), 'full', array( 'class' =>'img-fluid') ).'                     
+                  <div class="media-body pl-2">
+                    <h5 class="mt-0">' . get_the_title() .'</h5>
+                  </div>
                 </div>
-              </div>
-            </a>
-          </li>';
+              </a>
+            </li>';
+          }
         }
+        echo '</ul>';
       }
-      echo '</ul>';
-    }
-    wp_reset_query();
-  ?>
+      wp_reset_query();
+    ?>
+  <?php } ?>
 </div>
 
 
